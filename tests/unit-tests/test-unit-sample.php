de75d1291b1f8e0853423964948800d902f8b95a
Charles Olivier <charles.olivier@dreamsmiths.com>
2016-09-08 11:57:04 +0200

    Update

--- tests/unit-tests/test-unit-sample.php
+++ tests/unit-tests/test-unit-sample.php
@@ -1,16 +1,28 @@
 <?php
-namespace WCCT;
- 
-use phpmock\phpunit\PHPMock;
- 
-class UnitTests extends \PHPUnit_Framework_TestCase{
-    use PHPMock;
+/**
+ * Class UnitTests
+ *
+ * @package Wcct16
+ */
 
-	/**
-	 * A single example test.
-	 */
-	function test_wcct16_get_option() {
-		// Replace this with some actual testing code.
-		$this->assertEquals('river-club', wcct16_get_option('venue'));
+/**
+ * Sample test case.
+ */
+class UnitTests extends \PHPUnit_Framework_TestCase {
+	use PHPMock;
+
+	public function test_wcct_get_option() {
+		$get_option = $this->getFunctionMock('WCCT','get_option');
+
+		$get_option->expects($this->once())
+			->with($this->eqialTo('wcct_venue'))
+			->willReturn('river-club');
 	}
+
+	public function test_wcct_get_option() {
+		// Replace this with some actual testing code.
+		$this->assertEquals( 'river-club', wcct16_get_option('venue') );
+	}
+
+
 }
